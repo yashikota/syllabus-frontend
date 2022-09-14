8ba8441657a55f6cc9224ccd1a4f722d3254e1cb
kota <52403688+yashikota@users.noreply.github.com>
2022-09-14 17:57:12 +0900

    fix error

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,38 @@ import MaterialReactTable, {
   Virtualizer,
 } from 'material-react-table';
 import { SortingState } from '@tanstack/react-table';
-import { makeData, Person } from './makeData';
+import { faker } from '@faker-js/faker';
+
+export type Person = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  address: string;
+  zipCode: string;
+  city: string;
+  state: string;
+  country: string;
+  petName: string;
+  age: number;
+};
+
+export const makeData = (numberOfRows: number) =>
+  [...Array(numberOfRows).fill(null)].map(() => ({
+    firstName: faker.name.firstName(),
+    middleName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    phoneNumber: faker.phone.number(),
+    address: faker.address.streetAddress(),
+    zipCode: faker.address.zipCode(),
+    city: faker.address.city(),
+    state: faker.address.state(),
+    country: faker.address.country(),
+    petName: faker.animal.cat(),
+    age: faker.datatype.float({ min: 0, max: 100 }),
+  }));
 
 const Example: FC = () => {
   const columns = useMemo<MRT_ColumnDef<Person>[]>(
