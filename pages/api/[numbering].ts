96252382d1f3cdbe3d6456262b9dc465854aa1d7
kota <52403688+yashikota@users.noreply.github.com>
2022-09-15 19:46:33 +0900

    add api

--- pages/api/[numbering].ts
+++ pages/api/[numbering].ts
@@ -5,5 +5,14 @@ interface Request {
 }
 
 export default (req: Request, res: any) => {
-    res.status(200).json({ name: `John Doe ${req.query.numbering}` })
+    let url = "https://raw.githubusercontent.com/oit-tools/syllabus-scraping/master/data/2022.json"
+    fetch(url)
+        .then(res => res.json())
+        .then(json => {
+            let data = json[req.query.numbering]
+            if (data == undefined) {
+                res.status(404).json({ message: "Not Found" })
+            }
+            res.status(200).json(data)
+        })
 }
